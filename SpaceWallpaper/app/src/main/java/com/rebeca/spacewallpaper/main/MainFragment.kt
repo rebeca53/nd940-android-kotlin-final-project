358255c33e327137b83d8e8a6f0d1db392827e0b
rebeca <rebeca.n.rod@gmail.com>
2021-08-05 21:33:15 -0400

    Add navigation

--- SpaceWallpaper/app/src/main/java/com/rebeca/spacewallpaper/main/MainFragment.kt
+++ SpaceWallpaper/app/src/main/java/com/rebeca/spacewallpaper/main/MainFragment.kt
@@ -5,16 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.rebeca.spacewallpaper.databinding.FragmentMainBinding
 import com.rebeca.spacewallpaper.R
 
 class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_main,
+            container,
+            false)
+
+        binding.settingsButton.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToSettingsFragment())
+        }
+
+        binding.favoritesButton.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToFavoritesFragment())
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
